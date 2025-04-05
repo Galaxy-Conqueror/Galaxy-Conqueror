5a916146724b85f8d3b7635f6e69d7e04e3efbdd
Chris McHardy <chris.mchardy@bbd.co.za>
2025-04-05 17:21:29 +0200

    Add endpoint for login to return JWT, add appropriote models, clean up program file, setup structure with app config and services

---
+++ api/Galaxy.Conqueror.API/Services/GoogleAuthService.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Galaxy.Conqueror.API.Models;
+
+namespace Galaxy.Conqueror.API.Services;
+
+public class GoogleAuthService(HttpClient httpClient, IConfiguration configuration)
+{
+    private readonly HttpClient _httpClient = httpClient;
+    private readonly string _clientId = configuration["Google:ClientId"];
+    private readonly string _clientSecret = configuration["Google:ClientSecret"];
+    private readonly string _redirectUri = configuration["Google:RedirectUri"];
+
+    public async Task<TokenResponse> ExchangeAuthCodeForTokens (string authCode)
+    {
+        var requestData = new Dictionary<string, string>
+        {
+            { "code", authCode },
+            { "client_id", _clientId },
+            { "client_secret", _clientSecret },
+            { "redirect_uri", _redirectUri },
+            { "grant_type", "authorization_code" }
+        };
+
+        var requestContent = new FormUrlEncodedContent(requestData);
+        
+        var response = await _httpClient.PostAsync("https://oauth2.googleapis.com/token", requestContent);
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Token exchange failed: {response.StatusCode}, Body: {responseContent}");
+        }
+
+        return JsonSerializer.Deserialize<TokenResponse>(responseContent);
+    }
+}
