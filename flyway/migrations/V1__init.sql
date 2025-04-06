5feb01dba41b32eaac0dbb05ec788a5cb90f9aa5
Chris McHardy <chris.mchardy@bbd.co.za>
2025-04-06 02:00:05 +0200

    Update migrations to seed user data

--- flyway/migrations/V1__init.sql
+++ flyway/migrations/V1__init.sql
@@ -1,7 +1,13 @@
-CREATE TABLE IF NOT EXISTS planets (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    resources INT NOT NULL DEFAULT 100,
-    attack INT NOT NULL DEFAULT 1,
-    defense INT NOT NULL DEFAULT 1
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    google_id TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    username TEXT NOT NULL
 );
+
+INSERT INTO users (email, google_id, username) VALUES
+('commander1@galaxy.io', 'g123', 'CommanderOne'),
+('strategist2@galaxy.io', 'g456', 'StrategistTwo'),
+('explorer3@galaxy.io', 'g789', 'ExplorerThree');
