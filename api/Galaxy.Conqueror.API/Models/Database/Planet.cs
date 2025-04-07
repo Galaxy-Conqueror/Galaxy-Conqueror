c44a8ca860c1b23cb52940bf14a0ff035cc40cb7
Chris McHardy <chris.mchardy@bbd.co.za>
2025-04-07 16:56:20 +0200

    Add database models folder and first iteration of the model design

---
+++ api/Galaxy.Conqueror.API/Models/Database/Planet.cs
@@ -0,0 +1,11 @@
+﻿namespace Galaxy.Conqueror.API.Models.Database;
+
+public class Planet
+{
+    public int Id { get; set; }
+    public Guid UserId { get; set; }
+    public string? Name { get; set; }
+    public float? X { get; set; }
+    public float? Y { get; set; }
+}
+
---
+++ api/Galaxy.Conqueror.API/Models/Database/ResourceExtractor.cs
@@ -0,0 +1,9 @@
+﻿namespace Galaxy.Conqueror.API.Models.Database;
+
+public class ResourceExtractor
+{
+    public int Id { get; set; }
+    public int PlanetId { get; set; }
+    public int Level { get; set; }
+    public int Rate { get; set; }
+}
