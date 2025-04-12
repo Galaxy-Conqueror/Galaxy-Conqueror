f4c0ed7e7fed80519ade3d79602a9528ab876ae5
MnqobiNkabinde <mnqobi.nkabinde@bbd.co.za>
2025-04-12 17:00:04 +0200

    firstTest

--- terraform/fargate.tf
+++ terraform/fargate.tf
@@ -71,3 +71,35 @@ resource "aws_ecs_service" "ecs_service" {
   force_new_deployment = true
 
 }
+
+resource "aws_ecs_task_definition" "flyway_task" {
+  family                   = "flyway-task"
+  execution_role_arn       = aws_iam_role.ecs_task_exec_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+
+  container_definitions = jsonencode([
+    {
+      name  = "flyway"
+      image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.ecr_repo_name}:flyway"
+      essential = true
+      environment = [
+        { name = "FLYWAY_URL",     value = "jdbc:postgresql://${aws_db_instance.main.endpoint}/${aws_db_instance.main.db_name}" },
+        { name = "FLYWAY_USER",    value = local.db_creds.username },
+        { name = "FLYWAY_PASSWORD", value = local.db_creds.password }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_logs.name
+          awslogs-create-group  = "true"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "flyway"
+        }
+      }
+    }
+  ])
+}
