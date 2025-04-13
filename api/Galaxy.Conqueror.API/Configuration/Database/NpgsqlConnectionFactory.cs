bf8d3382a68c143e3c3e081807702d70a65bd2c1
MnqobiNkabinde <mnqobi.nkabinde@bbd.co.za>
2025-04-13 17:35:16 +0200

    removed sentitive infomation and add a conditional for getting the connection string

--- api/Galaxy.Conqueror.API/Configuration/Database/NpgsqlConnectionFactory.cs
+++ api/Galaxy.Conqueror.API/Configuration/Database/NpgsqlConnectionFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Npgsql;
 using System.Data.Common;
+using Microsoft.Extensions.Hosting;
 
 namespace Galaxy.Conqueror.API.Configuration.Database;
 
@@ -7,14 +8,21 @@ public class NpgsqlConnectionFactory : IDbConnectionFactory
 {
     private readonly string _connectionString;
 
-    public NpgsqlConnectionFactory()
+    public NpgsqlConnectionFactory(IConfiguration config, IHostEnvironment env)
     {
-        var host = Environment.GetEnvironmentVariable("DB_HOST");
-        var username = Environment.GetEnvironmentVariable("DB_USERNAME");
-        var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
-        var database = Environment.GetEnvironmentVariable("DB_NAME");
+        if (env.IsDevelopment())
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+        else
+        {
+            var host = Environment.GetEnvironmentVariable("DB_HOST");
+            var username = Environment.GetEnvironmentVariable("DB_USERNAME");
+            var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            var database = Environment.GetEnvironmentVariable("DB_NAME");
 
-        _connectionString = $"Host={host};Username={username};Password={password};Database={database}";
+            _connectionString = $"Host={host};Username={username};Password={password};Database={database}";
+        }
     }
 
     public DbConnection CreateConnection()
